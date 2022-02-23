9338918cae6a33d2c81621c5de5d848cdbf7b84c
IrisNordqvist <93259431+IrisNordqvist@users.noreply.github.com>
2022-02-23 13:38:03 +0100

    Diverse ändringar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const nav = document.getElementById('nav-links')
 const burger = document.getElementById('burger')
-
+ 
 function toggleMenu() {
-    nav.classlist.toggle('nav-active')
+    nav.classList.toggle('nav-active')
     burger.classList.toggle('crossed_line')
 }
-
+ 
 burger.addEventListener('click', toggleMenu)
